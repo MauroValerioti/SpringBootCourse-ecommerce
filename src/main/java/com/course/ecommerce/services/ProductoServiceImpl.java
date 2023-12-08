4eefcc086f08fcbf9b1f93e633ea9ba1e22ca490
Mauro <mauro.valerioti.18@gmail.com>
2023-12-08 11:10:08 -0300

    se creo el repository y las clases de servicio y el metodo show de productoController

---
+++ src/main/java/com/course/ecommerce/services/ProductoServiceImpl.java
@@ -0,0 +1,40 @@
+package com.course.ecommerce.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.course.ecommerce.model.Producto;
+import com.course.ecommerce.repository.ProductoRepository;
+
+//contiene la implementacion de los metodos de CRUD del service
+
+@Service
+public class ProductoServiceImpl implements ProductoService {
+
+	@Autowired
+	private ProductoRepository productoRepository;
+
+	@Override
+	public Producto save(Producto producto) {
+
+		return productoRepository.save(producto);
+	}
+
+	@Override
+	public Optional<Producto> get(Integer id) {
+		return productoRepository.findById(id);
+	}
+
+	@Override
+	public void update(Producto producto) {
+		productoRepository.save(producto);
+	}
+
+	@Override
+	public void delete(Integer id) {
+		productoRepository.deleteById(id);
+	}
+
+}
