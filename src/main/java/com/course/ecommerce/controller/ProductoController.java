38911fa2dee8c40a481df6a9e6cfc1044a531a59
Mauro <mauro.valerioti.18@gmail.com>
2023-12-11 09:00:53 -0300

    solucionando error de pagina de crear producto

--- src/main/java/com/course/ecommerce/controller/ProductoController.java
+++ src/main/java/com/course/ecommerce/controller/ProductoController.java
@@ -1,17 +1,19 @@
 package com.course.ecommerce.controller;
 
+import java.util.Optional;
+
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.course.ecommerce.model.Producto;
 import com.course.ecommerce.model.Usuario;
 import com.course.ecommerce.services.ProductoService;
-
 
 @Controller
 @RequestMapping("/productos")
@@ -26,10 +28,11 @@ public class ProductoController {
 	private ProductoService productoService;
 
 	@GetMapping("") // las comillas vacias van para que se mapee directamente hacia productos
-	public String show(Model model) { //model traera el elemento del backend hacia la vista
-		//contiene 2 parametros, el primer es el nombre con el que traere la info y el 2do es los elementos que voy a recibir
+	public String show(Model model) { // model traera el elemento del backend hacia la vista
+		// contiene 2 parametros, el primer es el nombre con el que traere la info y el
+		// 2do es los elementos que voy a recibir
 		model.addAttribute("productos", productoService.findAll());
-		
+
 		return "productos/show";
 	}
 
@@ -42,10 +45,21 @@ public class ProductoController {
 	public String save(Producto producto) {
 		LOGGER.info("Este es el objeto producto {}", producto);
 		Usuario u = new Usuario(1, "", "", "", "", "", "", "");
-		
+
 		producto.setUsuario(u);
-		
+
 		productoService.save(producto);
 		return "redirect:/productos";
+	}
+
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable Integer id) { //@PathVariable nos permite mapear la variable que viene a la url.
+		Producto producto = new Producto();
+		Optional<Producto> optionalProducto = productoService.get(id); //esto es lo que devuelve cuando hago la consulta por un elemento de tipo producto.
+		producto = optionalProducto.get();
+		
+		LOGGER.info("Producto buscado: {}", producto); //el LOGGER me permitira ver en la consola la variable que va a venir dentro de producto.
+		
+		return "productos/edit";
 	}
 }
